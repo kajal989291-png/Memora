32986aa9326dcf784470f643bcb2f999b3c322ed
kajal989291-png <kajal989291@gmail.com>
2026-09-23 12:37:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToTemplates() {
+    document.getElementById("templates").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function chooseBirthday() {
+    alert("🎂 Birthday Surprise selected! Customization page coming next ❤️");
+}
