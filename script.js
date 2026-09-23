70e52824b6c6723fb35044b9d76e31b1ad4f7451
kajal989291-png <kajal989291@gmail.com>
2026-09-23 12:47:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,76 @@ function scrollToTemplates() {
 }
 
 function chooseBirthday() {
-    alert("🎂 Birthday Surprise selected! Customization page coming next ❤️");
+    document.getElementById("home").style.display = "none";
+    document.getElementById("templates").style.display = "none";
+    document.getElementById("builder").style.display = "block";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 }
+
+function goHome() {
+    document.getElementById("builder").style.display = "none";
+    document.getElementById("home").style.display = "flex";
+    document.getElementById("templates").style.display = "block";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+function updatePreview() {
+    const name = document.getElementById("recipientName").value;
+
+    if (name.trim() === "") {
+        document.getElementById("previewName").innerText =
+            "Someone Special ❤️";
+    } else {
+        document.getElementById("previewName").innerText = name;
+    }
+
+    const message = document.getElementById("message").value;
+
+    if (message.trim() === "") {
+        document.getElementById("previewMessage").innerText =
+            "Your beautiful birthday message will appear here...";
+    } else {
+        document.getElementById("previewMessage").innerText = message;
+    }
+}
+
+function changeTheme(theme) {
+
+    const preview = document.getElementById("giftPreview");
+
+    preview.classList.remove(
+        "pink-theme",
+        "purple-theme",
+        "dark-theme"
+    );
+
+    preview.classList.add(theme + "-theme");
+}
+
+function previewPhoto(event) {
+
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+
+        const image = document.getElementById("previewPhoto");
+
+        image.src = e.target.result;
+        image.style.display = "block";
+
+    };
+
+    reader.readAsDataURL(file);
+            }
